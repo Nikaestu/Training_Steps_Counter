62bd41bda3887e5298f882bd582d77f5a198c279
clone <lehir-dylan@hotmail.com>
2024-08-19 10:47:52 +0200

    Initial Commit

---
+++ stepsCounter/stepsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepsCounterApp.swift
+//  stepsCounter
+//
+//  Created by Dylan Le Hir on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct stepsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
